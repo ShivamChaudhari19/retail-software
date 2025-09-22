4b867e1b64c5894494f60c8814604c550ed893ac
ShivamChaudhari19 <shivamsc7289@gmail.com>
2025-09-22 17:07:48 +0530

    solved error

--- src/main/java/in/shivamchaudhari/retail_software/controller/CategoryController.java
+++ src/main/java/in/shivamchaudhari/retail_software/controller/CategoryController.java
@@ -4,13 +4,15 @@ import in.shivamchaudhari.retail_software.io.CategoryRequest;
 import in.shivamchaudhari.retail_software.io.CategoryResponse;
 import in.shivamchaudhari.retail_software.service.CategoryService;
 import in.shivamchaudhari.retail_software.service.impl.CategoryServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryResponse addCategory(@RequestBody CategoryRequest request){
