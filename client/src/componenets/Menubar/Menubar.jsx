9e1e17c80c52461db6fb71fb2af50f4d79ad71b6
RohanBhaware <bhawarerohan24@gmail.com>
2025-11-03 21:04:07 +0530

    Dashboard

--- client/src/componenets/Menubar/Menubar.jsx
+++ client/src/componenets/Menubar/Menubar.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react'
 import './Menubar.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Logo from "../../assets/logo.png"
 import Profile from "../../assets/profile.png"
 import { AppContext } from '../../context/AppContext';
+
 const Menubar = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const {setAuthData} = useContext(AppContext);
     const logout = () => {
             localStorage.removeItem("token");
             localStorage.removeItem("role");
             setAuthData(null, null);
             navigate("/login");
+    }
+
+    const isActive = (path) => {
+        return location.pathname === path;
     }
 
   return (
@@ -25,22 +31,22 @@ const Menubar = () => {
     <div className="collapse navbar-collapse p-2" id="navbarNav">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                <Link className={`nav-link ${isActive('./dashboard') ?  'fw-bold text-warning': ''}`}to="/dashboard">Dashboard</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/explor">Explore</Link>
+                <Link className={`nav-link ${isActive('./explor') ?  'fw-bold text-warning': ''}`} to="/explor">Explore</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/items">Manage Items</Link>
+                <Link className={`nav-link ${isActive('./items') ?  'fw-bold text-warning': ''}`} to="/items">Manage Items</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/category">Manage Categories</Link>
+                <Link className={`nav-link ${isActive('./category') ?  'fw-bold text-warning': ''}`} to="/category">Manage Categories</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/users">Manage Users</Link>
+                <Link className={`nav-link ${isActive('./users') ?  'fw-bold text-warning': ''}`} to="/users">Manage Users</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/orders">Order History</Link>
+                <Link className={`nav-link ${isActive('./orders') ?  'fw-bold text-warning': ''}`} to="/orders">Order History</Link>
             </li>
         </ul>
         {/* <form className="d-flex">
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
 
 const Dashboard = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadData = async () => {
+      try{
+        const response = await fetchDashboardData();
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+        toast.error("Unable to view the data");
+      } finally{
+        setLoading(false);
+      }
+    }
+    loadData();
+  }, []);
+
+  if(loading){
+    return <div className='loading'>Loading dashboard...</div>
+  }
+
+  if(!data){
+    return <div className='error'>Failed to laod the dashboard data...</div>
+  }
+
   return (
-    <div>
-      Dashboard
+    <div className="dashboard-wrapper">
+      <div className='dashboard-container'>
+        <div className='start-grid'>
+          <div className='stat-card'>
+            <div className='stat-icon'>
+              <i className="bi bi-currency-rupee"></i>
+            </div>
+            <div className="stat-content">
+              <h3>Today's Sale</h3>
+              <p>₹{data.todaySales.toFixed(2)}</p>
+            </div>
+          </div>
+
+          <div className="stat-card">
+            <div className="stat-icon">
+              <i className="bi bi-cart-check"></i>
+            </div>  
+            <div className="stat-content">
+              <h3>Today's Orders</h3>
+              <p>{data.todayOrderCount}</p>
+            </div>
+          </div>
+        </div>
+        <div className="recent-orders-card">
+          <h3 className='recent-orders-tital'>
+            <i className="bi bi-clock-history"></i>
+            Recent Orders
+          </h3>
+          <div className="orders-table-container">
+            <table className='orders-table'>
+              <thead>
+                <tr>
+                  <th>Order Id</th>
+                  <th>Customer</th>
+                  <th>Amount</th>
+                  <th>Payment</th>
+                  <th>Status</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.recentOrders.map((order) => (
+                  <tr key={order.orderId}>
+                    <td>{order.orderId.substring(0,8)}...</td>
+                    <td>{order.customerName}</td>
+                    <td>₹{order.grandTotal.toFixed(2)}</td>
+                    <td>
+                      <span className={`payment-method ${order.paymentMethod.toLowerCase()}`}>
+                        {order.paymentMethod}
+                      </span>
+                    </td>
+                    <td>
+                      <span>
+                        {order.paymentMethod}
+                      </span>
+                    </td>
+                    <td>
+                      <span className={`status-badge ${order.paymentDetails.status.toLowerCase()}`}>
+                        {order.paymentDetails.status}
+                      </span>
+                    </td>
+                    <td>
+                      {new Date(order.createAt).toLowerCase([], {
+                        hover: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
